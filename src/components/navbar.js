388f764a920d24c62e3f1133011cf8f398337125
snyxe <153672410+Snyxex@users.noreply.github.com>
2026-01-11 20:55:25 +0100

    main

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,9 +4,11 @@ class AppNavbar extends HTMLElement {
          <nav class="navbar">
         <a href="index.html" class="logo">MyApp</a>
         <ul class="nav-links">
-          <li><a href="index.html">Home</a></li>
-          <li><a href="Kalender.html">Kalender</a></li>
-          <li><a href="guestbook.html">Gästebuch</a></li>
+          <li><a href="../../index.html">Home</a></li>
+          <li><a href="../../public/calender.html">Kalender</a></li>
+          <li><a href="../../public/guestbook.html">Gästebuch</a></li>
+          li>
+          <li><a href="../../public/news.html">Gästebuch</a></li>
         </ul>
       </nav>
       `;
