9c0fd43acabf11593a6a94c1df29f85e37246f7f
Jason <snyxexd@gmail.com>
2026-01-26 08:16:27 +0100

    Kalender
    fertiger kalender

--- src/db/query/get/getEvents.php
+++ src/db/query/get/getEvents.php
@@ -2,32 +2,12 @@
 header('Content-Type: application/json');
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
-// Wir suchen die Datei db.php dynamisch
-function findFile($filename, $startDir) {
-    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($startDir));
-    foreach ($iterator as $file) {
-        if ($file->getFilename() === $filename) {
-            return $file->getPathname();
-        }
-    }
-    return null;
-}
-
-// Suche im gesamten Projektordner (CMS_Verein)
-$projectRoot = '/Applications/XAMPP/xamppfiles/htdocs/CMS_Verein';
-$dbPath = findFile('db.php', $projectRoot);
-
-if ($dbPath) {
-    require_once $dbPath;
-} else {
-    die(json_encode(["error" => "db.php wurde im gesamten Projekt nicht gefunden!"]));
-}
+include '../../db.php';
 
 // Abfrage ausführen
 try {
     $sql = "SELECT event_id, title, event_date as date, location as room, description FROM events";
-    // Nutzt eure mysqli-Funktion
+    
     $result = query($connection, $sql); 
 
     $events = [];
